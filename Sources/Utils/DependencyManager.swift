1623a76c955d53aceafc8cc07e6b5b961b082d9b
Nam <nxnamvn@gmail.com>
2022-10-12 11:18:10 +0700

    update DependencyManager

---
+++ Sources/Utils/DependencyManager.swift
@@ -0,0 +1,33 @@
+//
+//  DependencyManager.swift
+//  
+//
+//  Created by Nam Nguyễn on 12/10/2022.
+//
+
+import UIKit
+
+open class DependencyManager {
+    fileprivate var factories = [String: Any]()
+    
+    public init() { }
+    
+    fileprivate func key<T>(_ type: T.Type) -> String {
+        return String(reflecting: type)
+    }
+    
+    public func register<T>(_ type: T.Type, factory: @escaping () -> T?) {
+        factories[key(type)] = factory
+    }
+    
+    public func unregister<T>(_ type: T.Type) {
+        factories[key(type)] = nil
+    }
+    
+    public func resolve<T>(_ type: T.Type) -> T? {
+        guard let factory = factories[key(type)] as? () -> T? else {
+            return nil
+        }
+        return factory()
+    }
+}
